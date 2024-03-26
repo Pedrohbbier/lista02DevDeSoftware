bd5e6d135cf75fcdc5c421b9579ee1054bb77938
Pedro Henrique Boldori Bier <124646793+Pedrohbbier@users.noreply.github.com>
2024-03-26 17:34:05 -0300

    Finished

--- Program.java
+++ Program.java
@@ -1,20 +1,55 @@
 public class Program {
     public static void main(String[] args) {
 
-        //Ex001.execute();
-        //Ex002.execute();
-        //Ex003.execute();
-        //Ex004.execute();
-        //Ex005.execute();
-        //Ex006.execute();
-        //Ex007.execute();
-        //Ex008.execute();
-        //Ex009.execute();
-        //Ex010.execute();
-        //Ex011.execute();
-        //Ex012.execute();
-        //Ex013.execute();
-        //Ex014.execute();
-        Ex015.execute();
+        int ex = Prompt.lerInteiro("Digite qual exerciço deseja executar: ");
+
+        switch (ex) {
+            case 1:
+                Ex001.execute();
+                break;
+            case 2:
+                Ex002.execute();
+                break;
+            case 3:
+                Ex003.execute();
+                break;
+            case 4:
+                Ex004.execute();
+                break;
+            case 5:
+                Ex005.execute();
+                break;
+            case 6:
+                Ex006.execute();
+                break;
+            case 7:
+                Ex007.execute();
+                break;
+            case 8:
+                Ex008.execute();
+                break;
+            case 9:
+                Ex009.execute();
+                break;
+            case 10:
+                Ex010.execute();
+                break;
+            case 11:
+                Ex011.execute();
+                break;
+            case 12:
+                Ex012.execute();
+                break;
+            case 13:
+                Ex013.execute();
+            case 14:
+                Ex014.execute();
+            case 15:
+                Ex015.execute();
+                break;    
+            default:
+                Prompt.imprimir("Opção inválida!");
+                break;
+        }
     }
 }
